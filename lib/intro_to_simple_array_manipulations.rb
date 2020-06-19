0bc03138d469a0dea214aa4ae5d9817df6d9deed
Anna Nicole Reed <reedanna@mail.com>
2020-06-19 17:24:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  deleted_elements = array.pop[2]
+  pry.binding
+end
+
+pop_with_args([1, 2, 3])
